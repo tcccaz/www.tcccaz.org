9e2b56104721df7d16519c1ca6cc2fd7818a8ce6
Joshua Chuang <joshua.chuang@gmail.com>
2015-10-02 01:46:53 -0700

    change back to github deploy

--- config.rb
+++ config.rb
@@ -106,19 +106,12 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-# activate :deploy do |deploy|
-#   deploy.method = :git
+activate :deploy do |deploy|
+  deploy.method = :git
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-# end
-
-activate :deploy do |deploy|
-  deploy.method   = :ftp
-  deploy.host            = 'ftp.tucsonchinesechristianchurch.org'
-  deploy.path            = '/'
-  deploy.user            = 'tcccweb'
-  deploy.password        = 'G0dIsL0ve316'
 end
+
